076be0ff65975a941948fc3c3c3f058e48ab9784
Arya Baladi <ariabaladi.ab@gmail.com>
2026-08-11 09:36:03 +0330

    Implement ToPersianDateString method

--- Hengam/Dates/PersianDateExtensions.cs
+++ Hengam/Dates/PersianDateExtensions.cs
@@ -1,6 +1,36 @@
-﻿namespace Hengam.Dates
+﻿using Hengam.Numbers;
+
+namespace Hengam.Dates
 {
-    public class PersianDateExtensions
+    public static class PersianDateExtensions
     {
+        private static readonly string[] PersianMonthNames =
+        {
+            "فروردین",
+            "اردیبهشت",
+            "خرداد",
+            "تیر",
+            "مرداد",
+            "شهریور",
+            "مهر",
+            "آبان",
+            "آذر",
+            "دی",
+            "بهمن",
+            "اسفند"
+        };
+
+        public static string ToPersianDateString(this DateTime dateTime)
+        {
+            var persianCalendar = new System.Globalization.PersianCalendar();
+
+            var year = persianCalendar.GetYear(dateTime);
+            var month = persianCalendar.GetMonth(dateTime);
+            var day = persianCalendar.GetDayOfMonth(dateTime);
+
+            return $"{PersianNumberFormatter.ToPersianDigits(day)} " +
+                   $"{PersianMonthNames[month - 1]} " +
+                   $"{PersianNumberFormatter.ToPersianDigits(year)}";
+        }
     }
 }
